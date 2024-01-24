af92a6454f02ae3816d855f8e445bc65a960efbb
Brusa99 <simone.brusatin@gmail.com>
2024-01-24 12:25:41 +0100

    completed monitoring

--- examples/satellite_monitor.py
+++ examples/satellite_monitor.py
@@ -1,5 +1,7 @@
 import numpy as np
-from src.monitoring import pos_bat_monitor, safe_bat_monitor, safe_alt_monitor, asym_stab_monitor, connection_monitor
+import matplotlib.pyplot as plt
+from src.monitoring import pos_bat_monitor, safe_bat_monitor, connection_monitor
+from src.monitoring import safe_lowalt_monitor, safe_highalt_monitor, lowasym_stab_monitor, highasym_stab_monitor
 
 
 def main():
@@ -7,7 +9,18 @@ def main():
     system_info = np.load("sat_info.npy", allow_pickle=True)
     n_satellites = system_info.shape[1]
 
-    alt_params = [[200e3, 1200e3], [200e3, 1200e3], [4500e3, 6000e3]]
+    # alt_params = [[200e3, 1200e3], [200e3, 1200e3], [4500e3, 6000e3]]
+    safe_alt_monitor = [safe_lowalt_monitor, safe_lowalt_monitor, safe_highalt_monitor]
+    asym_stab_monitor = [lowasym_stab_monitor, lowasym_stab_monitor, highasym_stab_monitor]
+
+    # for plotting
+    sat_names = ["Odyssey", "Rec Orbiter", "Relay"]
+    attr_names = ["connections", "altitude", "battery"]
+    colors = ["purple", "darkgreen", "orange"]
+    def extract(ls, i): return list(map(lambda x: x[i], ls))
+    fig_a, axs_a = plt.subplots(4, 1, gridspec_kw={'height_ratios': [4, 1, 1, 1]}, figsize=(16, 8), sharex=True)
+    fig_b, axs_b = plt.subplots(4, 1, gridspec_kw={'height_ratios': [4, 1, 1, 1]}, figsize=(16, 8), sharex=True)
+    fig_c, axs_c = plt.subplots(4, 1, gridspec_kw={'height_ratios': [4, 1, 1, 1]}, figsize=(16, 8), sharex=True)
 
     for sat_i in range(n_satellites):
         sat_info = system_info[:, sat_i]
@@ -19,11 +32,71 @@ def main():
         signals = list(zip(sat_connections, sat_altitude, sat_battery))
 
         pos_bat = np.array(pos_bat_monitor.monitor(time, signals))
-        safe_bat = np.array(safe_bat_monitor.monitor(time, signals, parameters=20.0))
-        # safe_alt = np.array(safe_alt_monitor.monitor(time, signals, parameters=alt_params[sat_i]))
-        # asym_stab = np.array(asym_stab_monitor.monitor(time, signals, parameters=alt_params[sat_i]))
+        safe_bat = np.array(safe_bat_monitor.monitor(time, signals))
         connection = np.array(connection_monitor.monitor(time, signals))
+        safe_alt = np.array(safe_alt_monitor[sat_i].monitor(time, signals))
+        # asym_stab = np.array(asym_stab_monitor[sat_i].monitor(time, signals))  # NOT WORKING
 
+        # plot altitude
+        axs_a[0].plot(time, sat_altitude, color=colors[sat_i], label=sat_names[sat_i], linewidth=0.5)
+        # extract
+        time2 = extract(safe_alt, 0)
+        values2 = extract(safe_alt, 1)
+        # time3 = extract(asym_stab, 0)
+        # values3 = extract(asym_stab, 1)
+        axs_a[sat_i + 1].plot(time2, values2, color="yellow", drawstyle="steps-post", label="Safe Altitude")
+        # axs[sat_i + 1].plot(time3, values3, color="blue", drawstyle="steps", label="Asymptotic Stability")
+        axs_a[sat_i + 1].set_ylim([-1.2, 1.2])
+        axs_a[sat_i + 1].set_ylabel(f"{sat_names[sat_i]}\nSatisfaction")
+        axs_a[sat_i + 1].legend()
+
+        # plot battery
+        axs_b[0].plot(time, sat_battery, color=colors[sat_i], label=sat_names[sat_i], linewidth=0.5)
+        # extract
+        time2 = extract(safe_bat, 0)
+        values2 = extract(safe_bat, 1)
+        axs_b[sat_i + 1].plot(time2, values2, color="yellow", drawstyle="steps-post", label="Safe Battery")
+        time3 = extract(pos_bat, 0)
+        values3 = extract(pos_bat, 1)
+        axs_b[sat_i + 1].plot(time3, values3, color="red", drawstyle="steps-post", label="Positive Battery")
+        axs_b[sat_i + 1].set_ylim([-1.2, 1.2])
+        axs_b[sat_i + 1].set_ylabel(f"{sat_names[sat_i]}\nSatisfaction")
+        axs_b[sat_i + 1].legend()
+
+        # plot connection
+        axs_c[0].plot(time, sat_connections, color=colors[sat_i], label=sat_names[sat_i], linewidth=0.5)
+        # extract
+        time2 = extract(connection, 0)
+        values2 = extract(connection, 1)
+        axs_c[sat_i + 1].plot(time2, values2, color="yellow", drawstyle="steps-post", label="Connection")
+        axs_c[sat_i + 1].set_ylim([-1.2, 1.2])
+        axs_c[sat_i + 1].set_ylabel(f"{sat_names[sat_i]}\nSatisfaction")
+        axs_c[sat_i + 1].legend()
+
+    # altitudes
+    axs_a[0].axhline(y=200e3, color="red", linestyle="--")
+    axs_a[0].axhline(y=1200e3, color="red", linestyle="--")
+    axs_a[0].axhline(y=4600e3, color="red", linestyle="--")
+    axs_a[0].axhline(y=5800e3, color="red", linestyle="--")
+    axs_a[0].set_ylabel("Altitude (m)")
+    axs_a[0].legend()
+    axs_a[3].set_xlabel("Time (min)")
+    fig_a.savefig("altituds.png")
+
+    # battery
+    axs_b[0].axhline(y=20, color="red", linestyle="--")
+    axs_b[0].axhline(y=0, color="red")
+    axs_b[0].set_ylabel("Battery (%)")
+    axs_b[0].legend()
+    axs_b[3].set_xlabel("Time (min)")
+    fig_b.savefig("battery.png")
+
+    # connection
+    axs_c[0].axhline(y=1, color="red", linestyle="--")
+    axs_c[0].set_ylabel("Connections")
+    axs_c[0].legend()
+    axs_c[3].set_xlabel("Time (min)")
+    fig_c.savefig("connection.png")
 
 
 if __name__ == "__main__":
--- src/monitoring.py
+++ src/monitoring.py
@@ -17,24 +17,29 @@ signal { int connections; real altitude; real battery; }
 domain boolean;
 formula PositiveBattery =  globally (battery > 0);
 formula SafeBatteryUsage(real sl) = eventually[0, 10] (battery > sl);
-formula SafeAltitude(real a_min, real a_max) = eventually[0,60] ((altitude > a_min) & (altitude < a_max));
+formula SafeAltitude(real a_min, real a_max) = eventually[0,20] ((altitude > a_min) & (altitude < a_max));
 formula AsymptoticStability(real a_min, real a_max) = eventually ( globally ((altitude > a_min) & (altitude < a_max)));
-formula Connection = ! globally (connections < 1);
+formula Connection = ! globally[0, 10] (connections < 1);
 """
 
-# other possibilities: (not working)
-# """
-# formula PositiveBattery =  globally (battery > 0);
-# formula SafeBatteryUsage(real sl) = globally (battery < sl => eventually (battery > sl));
-# formula SafeAltitude(real a_min, real a_max) = globally (altitude < a_min | altitude > a_max => eventually (altitude > a_min & altitude < a_max));
-# formula AsymptoticStability(real a_min, real a_max) = eventually ( globally (altitude > a_min & altitude < a_max));
-# formula Connection = ! globally (connections < 1);
-# """
+_parametrized_script = """
+signal { int connections; real altitude; real battery; }
+domain boolean;
+formula PositiveBattery =  globally (battery > 0);
+formula SafeBatteryUsage = eventually[0, 5] (battery > 20);
+formula SafeAltitudeLow = eventually[0,5] ((altitude > 200000) & (altitude < 1200000));
+formula SafeAltitudeHigh = eventually[0,5] ((altitude > 4600000) & (altitude < 5800000));
+formula AsymptoticStabilityLow = eventually ( globally ((altitude > 200000) & (altitude < 1200000)));
+formula AsymptoticStabilityHigh = eventually ( globally ((altitude > 4600000) & (altitude < 5800000)));
+formula Connection = ! globally[0, 30] (connections < 1);
+"""
 
-_moonlightScript = ScriptLoader.loadFromText(_script)
+_moonlightScript = ScriptLoader.loadFromText(_parametrized_script)
 
 pos_bat_monitor = _moonlightScript.getMonitor("PositiveBattery")
 safe_bat_monitor = _moonlightScript.getMonitor("SafeBatteryUsage")
-safe_alt_monitor = _moonlightScript.getMonitor("SafeAltitude")
-asym_stab_monitor = _moonlightScript.getMonitor("AsymptoticStability")
+safe_lowalt_monitor = _moonlightScript.getMonitor("SafeAltitudeLow")
+safe_highalt_monitor = _moonlightScript.getMonitor("SafeAltitudeHigh")
+lowasym_stab_monitor = _moonlightScript.getMonitor("AsymptoticStabilityLow")
+highasym_stab_monitor = _moonlightScript.getMonitor("AsymptoticStabilityHigh")
 connection_monitor = _moonlightScript.getMonitor("Connection")
